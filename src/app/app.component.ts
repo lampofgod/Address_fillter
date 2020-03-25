6bc6bf952b91717ef2d7725bcfeb87ff08f4deb0
lampofgod <akaka.k@hotmail.com>
2020-03-25 19:39:09 +0700

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {getProvince} from '../data/Province'
+import {getDistricts} from '../data/Districts'
+import {getSubDistricts} from '../data/SubDistricts'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ProvinceProject';
+  pro = getProvince();
+  dis = getDistricts();
+  sub = getSubDistricts();
+  // a = findDistricts(1)
+  disShow = this.dis
+  subDisShow = this.sub
+  
+  findDistricts(event) {
+    let findDis = this.dis
+    const result =  findDis.filter( (provinceName) => {
+      return provinceName.PROVINCE_ID == event.target.value
+    })
+    console.log(result)
+    this.disShow = result
+  }
+
+  findSubDistricts(event) {
+    let findSubDis = this.sub
+    const result =  findSubDis.filter( (disName) => {
+      return disName.DISTRICT_ID == event.target.value
+    })
+    console.log(result)
+    this.subDisShow = result
+  }
 }
